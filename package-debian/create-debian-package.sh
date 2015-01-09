f7a57b8e559d6640321ea4cecaa52b8ea928987e
Clément Nardi <clementnardi@gmail.com>
2015-01-09 16:00:17 +0100

    further modifications for debian packaging 4

--- package-debian/create-debian-package.sh
+++ package-debian/create-debian-package.sh
@@ -1,15 +1,31 @@
 
-current_dir=`pwd`
-script_dir=$current_dir/`dirname $0`/
-script_dir=`cd $script_dir;pwd`
+if git diff --quiet; then
+	current_dir=`pwd`
+	script_dir=$current_dir/`dirname $0`/
+	script_dir=`cd $script_dir;pwd`
 
 
-rcfile=$script_dir/../PicsDL/PicsDL.rc
-major=`grep MAJOR_VERSION $rcfile | cut -d " " -f3`
-minor=`grep MINOR_VERSION $rcfile | cut -d " " -f3`
-patch=`grep PATCH_VERSION $rcfile | cut -d " " -f3`
+	rcfile=$script_dir/../PicsDL/PicsDL.rc
+	major=`grep MAJOR_VERSION $rcfile | cut -d " " -f3`
+	minor=`grep MINOR_VERSION $rcfile | cut -d " " -f3`
+	patch=`grep PATCH_VERSION $rcfile | cut -d " " -f3`
 
-version=$major.$minor.$patch
-workingDir=Build-debian-package
+	version=$major.$minor.$patch
+	workingDir=$script_dir/Build-debian-package
+	packageName=pics-dl
+	upstreamTarballName=$packageName_$version.orig.tar.gz
 
-git archive --format tar.gz --prefix pics-dl-$version/ -o $workingDir/pics-dl_$version.orig.tar.gz HEAD
+	rm -rf $workingDir
+	mkdir $workingDir
+	git archive --format tar.gz --prefix $packageName-$version/ -o $workingDir/$upstreamTarballName HEAD
+	cd $workingDir
+	tar -zxvf $upstreamTarballName
+	cd $packageName-$version
+	debuild -us -uc
+
+
+else
+	echo "You need to commit you latest changes before creating the debian package"
+fi
+
+exit 0;
